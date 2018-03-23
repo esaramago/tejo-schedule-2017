58bf668955071002e130c5cc131718c87b1fc475
Emanuel Saramago <emanuel.reg@outlook.com>
2018-03-23 15:35:30 +0000

    homepage

--- src/js/main.js
+++ src/js/main.js
@@ -37,7 +37,7 @@ const App = {
 
         document.addEventListener('click', () => {
 
-            if (event.target.matches('.js-goto-schedule')) {
+            if (event.currentTarget.activeElement.matches('.js-goto-schedule')) {
                 this.goToSchedulePage();
             }
             else if (event.currentTarget.activeElement.matches('.js-goto-home')) {
@@ -101,15 +101,23 @@ const App = {
 
             template = template + `
                 <button type="button" class="c-card js-goto-schedule">
-                    <div class="c-card__section o-row c-itinerary">
+                    <div class="o-row c-itinerary">
                         <span>${inbound}</span>
                         <div class="o-row__wide">
                             <span class="c-itinerary__arrow"></span>
                         </div>
                         <span>${outbound}</span>
                     </div>
-                    <div class="u-text-center">
-                        <span class="u-font-semibold u-font-xl">${next}</span>
+                    <div class="o-row o-row--middle">
+                        <div class="o-row__wide c-time">
+                            <svg viewBox="0 0 32 32" class="c-icon--xl">
+                                <path d="M 16 4 C 9.382813 4 4 9.382813 4 16 C 4 22.617188 9.382813 28 16 28 C 22.617188 28 28 22.617188 28 16 C 28 9.382813 22.617188 4 16 4 Z M 16 6 C 21.535156 6 26 10.464844 26 16 C 26 21.535156 21.535156 26 16 26 C 10.464844 26 6 21.535156 6 16 C 6 10.464844 10.464844 6 16 6 Z M 15 8 L 15 17 L 22 17 L 22 15 L 17 15 L 17 8 Z "></path>
+                            </svg>
+                            <span>${next}</span>
+                        </div>
+                        <div>
+                            <span class="c-button">Horários</span>
+                        </div>
                     </div>
                 </button>
             `;
@@ -272,7 +280,7 @@ const App = {
         // scroll to current time schedule
         setTimeout(() => {
             document.querySelector(".is-next").scrollIntoView({ block: "center" });
-        }, 100);
+        }, 300);
     },
 
     goToSchedulePage() {
