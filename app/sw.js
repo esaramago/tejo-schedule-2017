2bd625b2c0dc9b08ffa82082d7c736f2fe2a0e85
Emanuel Saramago <emanuel.reg@outlook.com>
2018-04-22 17:36:49 +0100

    manifest fixes

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,7 @@
 //importScripts('/cache-polyfill.js');
+self.addEventListener('fetch', function (e) {
 
+});
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open('horariostejo').then(function (cache) {
